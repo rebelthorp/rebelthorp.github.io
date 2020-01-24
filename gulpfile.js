3ef098854afa40c14581fad2a98422937dac7de8
Yuriy Kochetov <ribbedcrown@gmail.com>
2020-01-24 15:16:30 +0700

    Bump 1.1.0
    Updated build env & add more products.

--- gulpfile.js
+++ gulpfile.js
@@ -7,77 +7,69 @@ const sourcemaps = require('gulp-sourcemaps');
 const csso = require('gulp-csso');
 const rename = require('gulp-rename');
 const versionNumber = require('gulp-version-number');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
-gulp.task('html', function () {
+gulp.task('html', function() {
   return gulp
-  .src('./src/*.hbs')
-  .pipe(hbs({
-    partials: './src/templates/partials/**/*.hbs',
-    helpers: './src/templates/helpers/*.js',
-    data: './src/templates/data/**/*.{js,json}'
-  }))
-  .pipe(rename({
-    extname: '.html'
-  }))
-  .pipe(htmlmin({
-    collapseWhitespace: true,
-    removeComments: true
-  }))
-  .pipe(versionNumber({
-    value: '%MDS%',
-    append: {
-      key: 'v',
-      to: ['css']
-    }
-  }))
-  .pipe(browserSync.reload({ stream: true }))
-  .pipe(gulp.dest('./'));
+    .src('./src/*.hbs')
+    .pipe(hbs({
+      partials: './src/templates/partials/**/*.hbs',
+      helpers: './src/templates/helpers/*.js',
+      data: './src/templates/data/**/*.{js,json}',
+    }))
+    .pipe(rename({
+      extname: '.html',
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+    }))
+    .pipe(versionNumber({
+      value: '%MDS%',
+      append: {
+        key: 'v',
+        to: ['css'],
+      },
+    }))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest('./'));
 });
 
-gulp.task('scss', function () {
+gulp.task('scss', function() {
   //noinspection JSUnresolvedFunction
   return gulp.src('./src/scss/**/*.scss')
-  .pipe(sass({
-    style: 'expanded',
-    includePaths: [
-      require('node-reset-scss').includePath,
-      'node_modules/sass-mq'
-    ]
-  }).on('error', sass.logError))
-  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-  .pipe(gulp.dest('./css'))
-  .pipe(sourcemaps.init())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(csso())
-  .pipe(sourcemaps.write('./'))
-  .pipe(browserSync.reload({ stream: true }))
-  .pipe(gulp.dest('./css'));
+    .pipe(sass({
+      style: 'expanded',
+      includePaths: [
+        require('node-reset-scss').includePath,
+        'node_modules/sass-mq',
+      ],
+    }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('./css'))
+    .pipe(sourcemaps.init())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(csso())
+    .pipe(sourcemaps.write('./'))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('browserSync', function () {
-  browserSync({
+gulp.task('watcher', function() {
+  browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './',
     },
     port: 8080,
     open: true,
-    notify: false
+    notify: false,
   });
+
+  gulp.watch('./src/**/*.hbs', gulp.series('html'));
+  gulp.watch('./src/**/*.json', gulp.series('html'));
+  gulp.watch('./src/scss/**/*.scss', gulp.series('scss'));
 });
 
-gulp.task('watcher', function () {
-  gulp.watch('./src/**/*.hbs', ['html']);
-  gulp.watch('./src/**/*.json', ['html']);
-  gulp.watch('./src/scss/**/*.scss', ['scss']);
-});
+gulp.task('build', gulp.series(gulp.parallel('html', 'scss')));
 
-gulp.task('build', [
-  'scss',
-  'html'
-]);
-
-gulp.task('default', [
-  'watcher',
-  'browserSync'
-]);
+gulp.task('default', gulp.series('watcher'));
